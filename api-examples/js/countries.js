8d23554088813b5b85e7875a1ec38d467bf6ac2b
dipdastoko <dipdastoko@gmail.com>
2021-10-05 14:28:45 +0600

    afaf

--- api-examples/js/countries.js
+++ api-examples/js/countries.js
@@ -13,13 +13,25 @@ const displayCountries = countries => {
     countries.forEach(country => {
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name.common;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.innerHTML = `
+        <h3>${country.name.common}</h3>
+        <p>${country.capital}</P>
+        <button onclick="loadCountryByName('${country.name.common}')">Datails</button>
+        `;
         countriesDiv.appendChild(div);
     })
 
+}
+
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]));
+
+}
+
+const displayCountryDetail = country => {
+    console.log(country);
 }
