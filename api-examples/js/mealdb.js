8ba3fa9969c5fb52abcd1fea09e51b259dc5516e
dipdastoko <dipdastoko@gmail.com>
2021-12-09 14:34:26 +0600

    enter key

--- api-examples/js/mealdb.js
+++ api-examples/js/mealdb.js
@@ -1,3 +1,11 @@
+const searchBtn = document.getElementById('button-search');
+const searchInput = document.getElementById('search-field');
+
+searchInput.addEventListener("keypress", function (event) {
+    // event.preventDefault();
+    if (event.key == 'Enter')
+        searchBtn.click();
+})
 
 const searchResult = document.getElementById('search-result');
 
